110613ebc611a88a10b8aa545899a9cc05e6701a
c43721 <55610086+c43721@users.noreply.github.com>
2020-10-05 00:13:57 -0500

    Animate the project page

--- js/projects.js
+++ js/projects.js
@@ -3,6 +3,9 @@ const pinnedProjects = $(".pinned-project");
 
 const viewMoreButtons = $(".view-more");
 
+const multiplier = Math.E * 1.02515;
+const contant = 25;
+
 viewMoreButtons.each(function () {
     $(this).on("click", function () {
         $(this).parentsUntil(".project-card").find(".view-more").text(function (i, text) {
@@ -12,15 +15,34 @@ viewMoreButtons.each(function () {
     })
 })
 
-pinnedProjects.each(function () {
-    $(this)
+pinnedProjects.each(function (i) {
+    $(this) //Copy each project and append to pinned section
         .clone()
         .removeClass(["pinned-project", "project-card"])
         .appendTo(".projects.pinned")
         .find("span.d-flex").removeClass(["d-flex", "justify-content-between"]).end()
         .find(".view-more").remove(".view-more").end()
-        .find('.hidden').removeClass(["hidden", "project-card"]).addClass("show")
+        .find('.hidden').removeClass(["hidden", "project-card"]).addClass("show").end()
 })
 
 const pinnedProjectsContainer = $(".projects.pinned").children().toArray().slice(1);
 if (!pinnedProjectsContainer.length) projectResultSpan.text("No Pinned Projects");
+
+const projects = $("#project-container");
+
+function loadingAnimations() {
+    projects.each(function () {
+        $(this).children().each(function (i) {
+            const delayFactor = getDelay(i);
+            $(this)
+                .addClass("project-loading")
+                .css("animation-delay", delayFactor + "s");
+        })
+    })
+}
+
+function getDelay(x) {
+    return `.${x + 1}`
+}
+
+projects.on("load", loadingAnimations());
