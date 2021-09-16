1728d187f78de035d21f830dc4dcfea9ff2e4e6e
c43721 <rlahman.game@gmail.com>
2021-09-16 08:37:47 -0500

    feat: active link color changes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	interface Link {
 		name: string;
 		path: string;
 	}
+
+	let active = (path: string) => $page.path === path;
 
 	let links: Link[] = [
 		{ name: 'Home', path: '/' },
@@ -12,8 +15,10 @@
 </script>
 
 <div class="container">
+	<a class="link__header" href="/">c43721</a>
+
 	{#each links as link (link.name)}
-		<a class="link" href={link.path}>{link.name}</a>
+		<a class="link" class:link--active={active(link.path)} href={link.path}>{link.name}</a>
 	{/each}
 </div>
 
@@ -21,19 +26,40 @@
 	@import '../styles/base';
 
 	$padding: 1rem;
-	$containerHeight: 75px;
+	$containerHeight: 50px;
+
+	$colorActive: rgba(0, 0, 0, 0.9);
+	$colorInactive: rgba(0, 0, 0, 0.5);
+
 	.container {
 		height: $containerHeight;
 		background-color: $primary;
+		display: flex;
+		align-items: center;
+		padding: 0 5vw;
 	}
 
 	.link {
 		padding: $padding;
-		color: $textSecondary;
+		color: $colorInactive;
 		text-decoration: none;
+		margin: 0 8px;
+
+		&:hover {
+			color: $textPrimary;
+		}
 
 		&--active {
-			color: $textPrimary;
+			color: $colorActive;
+			font-weight: 400;
+		}
+
+		&__header {
+			color: $colorActive;
+			padding: $padding;
+			text-decoration: none;
+			padding: 3px 7px;
+			font-weight: 600;
 		}
 	}
 </style>
