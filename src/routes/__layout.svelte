6098e979c2beb6fc0dfe494f5fab5d518cddd717
c43721 <rlahman.game@gmail.com>
2021-09-18 15:34:59 -0500

    feat: use layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Confetti from '../components/Water.svelte';
+	import Header from '../components/Header.svelte';
+</script>
+
+<Header />
+
+<slot />
+
+<Confetti />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,18 @@
 <script>
 	import InfoBox from '../components/InfoBox.svelte';
-	import Confetti from '../components/Water.svelte';
-	import Header from '../components/Header.svelte';
 </script>
-
-<Header />
 
 <main class="content">
 	<InfoBox />
 </main>
-
-<Confetti />
-
-<div class="wave" />
 
 <style lang="scss">
 	.content {
 		margin: 0 1rem;
 		display: flex;
 		justify-content: center;
-        align-items: center;
-        height: 45vh;
-        min-width: 50vw;
+		align-items: center;
+		height: 45vh;
+		min-width: 50vw;
 	}
 </style>
